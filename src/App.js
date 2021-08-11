96ce7140d668f611cf6c2be1177a431816cfe4c0
Ross Foy <hamzaoflouisville@gmail.com>
2021-08-11 17:18:47 -0400

    saving progress.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CreatePsap from './components/CreatePsap';
+import ShowPsapList from './components/ShowPsapList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowPsapList} />
+          <Route path='/create-psap' component={CreatePsap} />
+        </div>
+      </Router>
+    );
+  }
 }
+  
+
 
 export default App;
---
+++ src/components/CreatePsap.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import axios from 'axios';
+
+
+
+class CreatePsap extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            county: '',
+            state: '',
+            phone_number: ''
+        }
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const data = {
+            county: this.state.county,
+            state: this.state.state,
+            phone_number: this.state.phone_number
+        }
+
+        axios.post('http://localhost:8082/psaps', data)
+        .then(res => {
+            this.setState({
+                county: '',
+                state: '',
+                phone_number: ''
+            })
+            this.props.history.push('/')
+        })
+        .catch(err => {
+            console.log(`Error in CreatePsap!`)
+        })
+    }
+
+    render() {
+        return(
+            <div className="CreatePsap">
+                <div className="create-psap-container">
+                    <div className="row">
+                        <div className="col-mid-8 m-auto">
+                            <br />
+                            <Link to="/" className="btn btn-outline-warning float-left">
+                                Show Book List
+                            </Link>
+                        </div>
+                        <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Add Book</h1>
+              <p className="lead text-center">
+                  Create new book
+              </p>
+
+              <form noValidate onSubmit={this.onSubmit}>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='County of PSAP'
+                    name='county'
+                    className='form-control'
+                    value={this.state.county}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <br />
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='The state which the county belongs to. . .'
+                    name='state'
+                    className='form-control'
+                    value={this.state.state}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='number'
+                    placeholder='PSAP Number'
+                    name='phone_number'
+                    className='form-control'
+                    value={this.state.phone_number}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <input
+                    type="submit"
+                    className="btn btn-outline-warning btn-block mt-4"
+                />
+              </form>
+          </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default CreatePsap;
