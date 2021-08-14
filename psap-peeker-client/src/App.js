42b9a8d41354156fff3029dfb7de1f1c20043015
Ross Foy <hamzaoflouisville@gmail.com>
2021-08-14 05:46:58 -0400

    begin work on the SearchPsap route.

--- psap-peeker-client/src/App.js
+++ psap-peeker-client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CreatePsap from './components/CreatePsap';
 import ShowPsapList from './components/ShowPsapList';
+import SearchPsap from './components/SearchPsap';
 
 
 
@@ -13,6 +14,7 @@ class App extends Component {
         <div>
           <Route exact path='/' component={ShowPsapList} />
           <Route path='/create-psap' component={CreatePsap} />
+          <Route path="/search-psap" component={SearchPsap} />
         </div>
       </Router>
     );
