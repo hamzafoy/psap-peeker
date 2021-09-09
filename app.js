9f9d5e8c7b3a7cce6d0fde998a7d603be052b6f8
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-09 16:03:51 -0400

    attempt to fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.enable('trust proxy');
 app.use(express.json({ extended: false }));
 app.use(routes);
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
     // Express will serve up production assets
     app.use(express.static("build"));
   
@@ -23,6 +23,19 @@ if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) =>
       res.sendFile(path.resolve("build", "index.html"))
     );
+  } */
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+  }
+  
+  else {
+    app.use(express.static(path.join(__dirname, '/client/public')));
+    app.get("/*", function(req, res) {
+      res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    });
   }
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
