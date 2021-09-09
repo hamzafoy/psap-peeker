c8b285e2fc81f9fa9b30f4084e121842f98eeda9
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-09 11:15:08 -0400

    update entire repo to be able to deploy as fullstack via Heroku only.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const connectToDatabase = require('./config/db');
 let cors = require('cors');
+const path = require("path");
 const app = express();
 const routes = require('./routes/psaps')
 const port = process.env.PORT || 8082;
@@ -9,10 +10,21 @@ const port = process.env.PORT || 8082;
 
 connectToDatabase();
 
-
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 app.use(routes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 /* "build": "CI=false && react-scripts build", */
+/*--
+,
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2",
+    "react-router-dom": "^5.2.0",
+    "react-scripts": "4.0.3"
+--*/
