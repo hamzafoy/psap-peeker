273cdf7d5727ce9061146602bc146d490486eb84
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-10 13:04:44 -0400

    fixing prod route on app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("/*", function(req, res) {
+    app.get("*", function(req, res) {
       res.sendFile(path.join(__dirname, "./client/build/index.html"));
     });
   }
