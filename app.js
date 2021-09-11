b3fc31ac9837838da8f225a45f230ea320706e6b
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-10 22:29:59 -0400

    test

--- app.js
+++ app.js
@@ -11,16 +11,15 @@ const port = process.env.PORT || 8082;
 connectToDatabase();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
-
-app.use('/api', routes);
 app.use('/', express.static(path.join(__dirname, '/client/build')));
+app.use('/api', routes);
+
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
