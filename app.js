0363c632d30031ead3d57a55bbb4a2ff87ea97b7
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-11 00:14:35 -0400

    clean up the root's  folder in the backend.

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
+/*--
+Requiring all needed dependencies including Express for the back-end, cors to aid HTTP requests,
+path to help with running React from the backend, utilizing modularized routes, & more.
+--*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const connectToDatabase = require('./config/db');
-let cors = require('cors');
+const cors = require('cors');
 const path = require("path");
 const app = express();
 const routes = require('./routes/psaps')
 const port = process.env.PORT || 8082;
 
 
+
+/*--
+Connecting to MongoDB database, listing what actions the backend will need to facilitate.
+--*/
+
 connectToDatabase();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 app.use('/', express.static(path.join(__dirname, '/client/build')));
+/*- app.use('*', express.static(path.join(__dirname, "client", "build"))) -*/
 app.use('/api', routes);
 
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
 
-  // Express serve up index.html file if it doesn't recognize route
+/*--
+TASK: Need to investigate why Heroku is not having the front end utilize ./client/build
+--*/
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
 
-/* "build": "CI=false && react-scripts build", */
-/*--
-,
-    "react": "^17.0.2",
-    "react-dom": "^17.0.2",
-    "react-router-dom": "^5.2.0",
-    "react-scripts": "4.0.3"
---*/
+app.listen(port, () => console.log(`Server running on port ${port}`));
